ce7b6ac27250ebcadadd43b16f773718ce5b10d6
bnsnchz <bsanchez4488@gmail.com>
2018-05-22 21:16:31 -0700

    added initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    console.log( "ready!" );
+});
+
+var questions = {
+    "Who sang the song September?":"Earth Wind and Fire",
+    "Who sang the song Mr. Blue Sky?":"Electric Light Orchestra",
+    "Who sang the song I Will Survive?":"Gloria Gaynor",
+    "Who sang the song Lets Get It On?":"Marvin Gaye",
+    "Who sang the song Baby Come Back?":"Player",
+}
+
